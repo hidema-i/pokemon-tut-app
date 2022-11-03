f4f7607190110ac88dc5aa16430414f23eaea5f3
ShuumaIwamoto <iwa06ki12da@gmail.com>
2022-11-04 01:08:59 +0900

    detail表示

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,20 @@ interface Pokemons {
   url: string;
 }
 
+export interface Detail {
+  id: number;
+  isOpened: boolean;
+}
+
 const App: React.FC = () => {
   //pokemonを配列で保持
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [nextUrl, setNextUrl] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const [viewDetail, setDetail] = useState<Detail>({
+    id: 0,
+    isOpened: false,
+  });
   //axiosで取得したデータを一度だけとる
   useEffect(() => {
     //非同期処理
@@ -19,21 +30,47 @@ const App: React.FC = () => {
       const res = await axios.get(
         "https://pokeapi.co/api/v2/pokemon?limit-20&offset-20"
       );
+      setNextUrl(res.data.next);
       res.data.results.forEach(async (pokemon: Pokemons) => {
         const poke = await axios.get(
           `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
         );
         setPokemons((p) => [...p, poke.data]);
+        setLoading(false);
       });
     };
     getpokemon();
   }, []);
 
+  const nextPage = async () => {
+    setLoading(true);
+    let res = await axios.get(nextUrl);
+    setNextUrl(res.data.next);
+    res.data.results.forEach(async (pokemon: Pokemons) => {
+      const poke = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+      );
+      setPokemons((p) => [...p, poke.data]);
+      setLoading(false);
+    });
+  };
+
   return (
     <div className="App">
       <div className="container">
         <header className="pokemon-header"> Pokemon</header>
-        <PokemonCollection pokemons={pokemons} />
+        <PokemonCollection
+          pokemons={pokemons}
+          viewDetail={viewDetail}
+          setDetail={setDetail}
+        />
+        {!viewDetail.isOpened && (
+          <div className="btn">
+            <button onClick={nextPage}>
+              {loading ? "Loading..." : "Load more"}{" "}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/PokemonCollection.tsx
+++ src/components/PokemonCollection.tsx
@@ -1,30 +1,56 @@
 import React from "react";
-import { Pokemon } from "../interface";
+import { PokemonDetail } from "../interface";
 import PokemonList from "../PokemonList";
 import "./pokemon.css";
+import { Detail } from "../App";
+
 interface Props {
-  pokemons: Pokemon[];
+  pokemons: PokemonDetail[];
+  viewDetail: Detail;
+  setDetail: React.Dispatch<React.SetStateAction<Detail>>;
 }
 
 const PokemonCollection: React.FC<Props> = (props) => {
-  const { pokemons } = props;
+  const { pokemons, viewDetail, setDetail } = props;
+  const selectPokemon = (id: number) => {
+    if (!viewDetail.isOpened) {
+      setDetail({
+        id: id,
+        isOpened: true,
+      });
+    }
+  };
   return (
-    <div>
-      <section className="collection-container">
+    <>
+      <section
+        className={
+          viewDetail.isOpened
+            ? "collection-container-active"
+            : "collection-container"
+        }
+      >
+        {viewDetail.isOpened ? (
+          <div className="overlay"></div>
+        ) : (
+          <div className=""></div>
+        )}
         {pokemons.map((pokemon) => {
           return (
-            <div className="">
+            <div onClick={() => selectPokemon(pokemon.id)}>
               <PokemonList
+                viewDetail={viewDetail}
+                setDetail={setDetail}
                 key={pokemon.id}
                 name={pokemon.name}
                 id={pokemon.id}
+                abilities={pokemon.abilities}
                 image={pokemon.sprites.front_default}
               />
             </div>
           );
         })}
       </section>
-    </div>
+    </>
   );
 };
 
